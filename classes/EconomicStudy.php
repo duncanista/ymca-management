19bf953d74c714521b2a58eb24183c3df7231f7d
luisalcantaralo <alcantaraluisalfonso@gmail.com>
2019-04-29 10:20:25 -0500

    economic

---
+++ classes/EconomicStudy.php
@@ -0,0 +1,12 @@
+<?php
+  require_once('Database.php');
+
+  Class EconomicStudy{
+    $fields = array("idStudy", "idStudent", "lastname", "housingType", "housingTenure", "dimensions", "walls", "roofs", "floors",
+  "rooms", "lightbulbs", "condition", "familyCategory", "familyAlimentation", "totalIncome", "totalOutcome", "healthService", "vacations");
+
+    function __construct(){
+    }
+  }
+
+ ?>
